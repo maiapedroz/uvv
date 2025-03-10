4cc4699b69afd84be085ef30156ba0fd27f5ad1d
Pedro Maia <126162918+maiapedroz@users.noreply.github.com>
2025-03-10 08:43:14 -0300

    Add files via upload

---
+++ 5periodo/webavancado/aula/README.md
@@ -0,0 +1,14 @@
+###Comandos para rodar o app web atraves de um ambiente virtual:
+
+
+''py -m venv venv''
+''Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser''
+''venv\Scripts\activate''
+''py -m pip install --upgrade pip''
+''pip install flask''
+''pip install ipython''
+''pip install flask-wtf''
+''set FLASK_APP=bloguvv.py''
+''$env:FLASK_DEBUG = 1''
+''flask run''
+
--- 5periodo/webavancado/aula/app/routes.py
+++ 5periodo/webavancado/aula/app/routes.py
@@ -2,6 +2,7 @@
 
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +19,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts = posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
